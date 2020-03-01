ae638cdd8a2727f4eb2196c5ecf66486dc482a83
Muthusamy Manigandan <mmaniga@yahoo.co.uk>
2020-03-01 22:41:27 +0530

    adding csv output

--- src/main/java/com/seldon/mih/mr/FileUtil.java
+++ src/main/java/com/seldon/mih/mr/FileUtil.java
@@ -1,5 +1,7 @@
 package com.seldon.mih.mr;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -7,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -24,10 +27,26 @@ public class FileUtil {
         fileWriter.close();
     }
 
+    static String[] stopWords = {
+            "I","me", "my",
+            "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself",
+            "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are",
+            "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as",
+            "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to",
+            "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all",
+            "any", "both", "each", "few", "more", "most","other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can",
+            "will", "just", "don", "should", "now",
+    };
+
+    static Set<String > stopWordsSet = new HashSet<>(Arrays.asList(stopWords));
+
+
     public static Map<String,Integer> toFrequencyMap(String filePath) throws IOException {
         Path path = Paths.get(filePath);
-        Map<String, Integer> wordCount = Files.lines(path).flatMap(line -> Arrays.stream(line.trim().split("\\s+")))
+        Map<String, Integer> wordCount = Files.lines(path)
+                .flatMap(line -> Arrays.stream(line.trim().split("\\s+")))
                 .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
+                .filter(word -> !stopWordsSet.contains(word))
                 .filter(word -> word.length() > 0)
                 .map(word -> new AbstractMap.SimpleEntry<>(word, 1))
                 .collect(toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2));
@@ -35,6 +54,25 @@ public class FileUtil {
         return wordCount;
     }
 
+    public static int countOfLinesInFile(String filePath) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        AtomicInteger lines = new AtomicInteger();
+        while (reader.readLine() != null) lines.getAndIncrement();
+        reader.close();
+        return lines.get();
+    }
+
+
+    public static Map<String,Integer> toTermFrequencyMap(String filePath) throws IOException {
+        int noOfWordsInDoc = countOfLinesInFile(filePath);
+        Path path = Paths.get(filePath);
+        Map<String, Integer> wordCount = Files.lines(path)
+                .flatMap(line -> Collections.)
+                .map(word -> new AbstractMap.SimpleEntry<>(word, 1))
+                .collect(toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2));
+
+        return wordCount;
+    }
     public static void printFrequency(Map<String,Integer> wordCount) {
         wordCount.forEach((k, v) -> System.out.println(String.format("%s ==>> %d", k, v)));
     }
